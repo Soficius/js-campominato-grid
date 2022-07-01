3b2c7d9d9dfc6ebb22bf9cb3bed7be0e3cdbaa0e
Soficius <103771506+Soficius@users.noreply.github.com>
2022-07-01 12:09:32 +0200

    fixed colour function and added bonus files

--- js/bonus.js
+++ js/bonus.js
@@ -1,7 +1,7 @@
 //* 1.3. funzione per creare una celle
-function getNewCell(num){
+function getNewCell(num, gridSize){
     const cell = document.createElement('div');
-    cell.className = 'cell'
+    cell.className = 'cell' + gridSize
     cell.innerText = num
     cell.addEventListener('click',colorizeCell)
     return cell   
@@ -20,20 +20,28 @@ function colorizeCell(e){
 document.getElementById('start').addEventListener('click', function(){
     //* 1.1. prendo l'elemento griglia
     const grid = document.getElementById('grid');
+    const level = document.getElementById('level');
     //* rimuovo eventuali celle preesistenti
     while (grid.firstChild) {
         grid.removeChild(grid.lastChild)
     }
-    //* 1.2. devo creare una griglia di 100 celle divisa in 10 colonne e 10 righe 
-    const rows = 10;
-    const cols = 10;
-    
-    const total = rows * cols;
-    
-    //* 1.4. creo 100 celle
+    //* 1.2. devo creare una griglia di celle
+    let total = 0
+    switch(level.value){
+        case '10x10': 
+            total = 10*10
+            break
+        case '9x9':
+            total= 9*9
+            break
+        case '7x7':
+            total = 7*7
+            break
+    }
+    //* 1.4. creo celle in base al livello
     for(let i = 1; i <= total; i++ ){
         // 1.4.1 aggancio la cella alla griglia
-        grid.appendChild(getNewCell(i))
+        grid.appendChild(getNewCell(i, level.value))
     }
 })
 
